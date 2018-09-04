5757ef02cf3d7aeb9e78f14ba86fb65bf838d0d6
juan-ONE <40164608+juan-ONE@users.noreply.github.com>
2018-09-04 19:04:55 +0200

    edit routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -8,12 +8,13 @@ $route['register_individual'] = 'IndividualController/register_individual';
 $route['donate_now'] = 'IndividualController/donate_now';
 $route['logged_in'] = 'IndividualController/logged_in';
 $route['social'] = 'IndividualController/social';
-$route['homepage2'] = 'IndividualController/homepage2';
+
 $route['donatemore'] = 'IndividualController/donatemore';
 
 $route['company'] = 'CompanyController/index';
 $route['addCompany'] = 'CompanyController/addCompany';
 $route['coordinate'] = 'CoordinateController';
+$route['storecoords'] = 'admin/koordinatjembatan/storeCoordsByDonor';
 $route['show'] = 'MapController/show';
 $route['donation/show/(:num)'] = 'MapController/donation/$1';
 $route['map'] = 'MapController';
